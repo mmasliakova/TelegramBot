36ca6b7e67d2b9e8a84ddc14738d232286830a59
mimasliakova <mimasliakova@users.noreply.github.com>
2020-05-02 02:54:12 +0300

    kotik.py

---
+++ kotik.py
@@ -0,0 +1,33 @@
+import telebot
+from telebot import types
+import argparse
+from random import randrange
+
+import os
+
+
+bot = telebot.TeleBot("1145880562:AAFvfDIwCdjGEmJPizW6f7fPbfPLVheD6n8")
+parser = argparse.ArgumentParser()
+parser.add_argument('--pictures_path', type=str, help='Путь к папке с картинками', default='pics/')
+args = parser.parse_args()
+picture_files = os.listdir(args.pictures_path)
+picture_files = [os.path.join(args.pictures_path, i) for i in picture_files]
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    chat_id = message.chat.id
+    markup = types.ReplyKeyboardMarkup()
+    item = types.KeyboardButton('/Получить')
+    markup.row(item)
+    bot.send_message(chat_id, "Привет! Хочешь кота?", reply_markup=markup)
+
+
+@bot.message_handler(commands=['Получить'])
+def echo_all(message):
+    chat_id = message.chat.id
+    i = randrange(len(picture_files))
+    bot.send_photo(chat_id, open(picture_files[i], 'rb'))
+
+
+bot.polling()
